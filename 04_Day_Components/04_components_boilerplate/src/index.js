89240edc06260afc91cf805df966b1998af6b67a
thienhnguyen <thienhnguyen@kms-technology.com>
2023-02-03 19:04:28 +0700

    update

--- 04_Day_Components/04_components_boilerplate/src/index.js
+++ 04_Day_Components/04_components_boilerplate/src/index.js
@@ -1,42 +1,43 @@
 // index.js
-import React from 'react'
-import ReactDOM from 'react-dom'
-import asabenehImage from './images/asabeneh.jpg'
+import React from "react";
+import ReactDOM from "react-dom";
+import asabenehImage from "./images/asabeneh.jpg";
 
 const hexaColor = () => {
-  let str = '0123456789abcdef'
-  let color = ''
+  let str = "0123456789abcdef";
+  let color = "";
   for (let i = 0; i < 6; i++) {
-    let index = Math.floor(Math.random() * str.length)
-    color += str[index]
+    let index = Math.floor(Math.random() * str.length);
+    color += str[index];
   }
-  return '#' + color
-}
+  return "#" + color;
+};
 
 const HexaColor = () => {
-  const bgColor = hexaColor()
+  const bgColor = hexaColor();
   const styles = {
-    height: '100px',
-    display: 'flex',
-    'justify-content': 'center',
-    'align-items': 'center',
-    fontFamily: 'Montserrat',
-    margin: '2px auto',
-    borderRadius: '5px',
-    width: '75%',
-    border: '2px solid black',
-  }
+    height: "100px",
+    display: "flex",
+    "justify-content": "center",
+    "align-items": "center",
+    fontFamily: "Montserrat",
+    margin: "2px auto",
+    borderRadius: "5px",
+    width: "75%",
+    border: "2px solid white",
+    backgroundColor: bgColor,
+  };
   return (
     <div style={styles}>
       <h2>{bgColor}</h2>
     </div>
-  )
-}
+  );
+};
 
 // Header Component
 const Header = () => (
   <header>
-    <div className='header-wrapper'>
+    <div className="header-wrapper">
       <h1>Welcome to 30 Days Of React</h1>
       <h2>Getting Started React</h2>
       <h3>JavaScript Library</h3>
@@ -44,67 +45,69 @@ const Header = () => (
       <small>Oct 3, 2020</small>
     </div>
   </header>
-)
+);
 
-// User Card Component
-const UserCard = () => (
-  <div className='user-card'>
-    <img src={asabenehImage} alt='asabeneh image' />
-    <h2>Asabeneh Yetayeh</h2>
-  </div>
-)
+// // User Card Component
+// const UserCard = () => (
+//   <div className="user-card">
+//     <img src={asabenehImage} alt="asabeneh image" />
+//     <h2>Asabeneh Yetayeh</h2>
+//   </div>
+// );
+
+const buttonStyles = {
+  padding: "10px 20px",
+  marginLeft: "10px",
+  background: "#2ACFCF",
+  border: "none",
+  borderRadius: 5,
+  color: "white",
+};
+
+// const Button = () => <button style={buttonStyles}> action </button>;
 
 // TechList Component
 const TechList = () => {
-  const techs = ['HTML', 'CSS', 'JavaScript']
-  const techsFormatted = techs.map((tech) => <li key={tech}>{tech}</li>)
-  return techsFormatted
-}
-const buttonStyles = {
-  padding: '10px 20px',
-  background: 'rgb(0, 255, 0)',
-  border: 'none',
-  borderRadius: 5,
+  const techs = ["HTML", "CSS", "Javascript"]
+  const formatted = techs.map((tech) => <button style={buttonStyles}>{tech}</button>)
+  return formatted
 }
 
-const Button = () => <button style={buttonStyles}> action </button>
+// // Main Component
+// const Main = () => (
+//   <main>
+//     <div className="main-wrapper">
+//       <p>Prerequisite to get started react.js:</p>
+//       <ul>
+//         <TechList />
+//       </ul>
+//       <UserCard />
+//       <div>
+//         {/* Generate two different hexa colors every time */}
+//         <HexaColor />
+//         <HexaColor />
+//       </div>
+//     </div>
+//   </main>
+// );
 
-// Main Component
-const Main = () => (
-  <main>
-    <div className='main-wrapper'>
-      <p>Prerequisite to get started react.js:</p>
-      <ul>
-        <TechList />
-      </ul>
-      <UserCard />
-      <div>
-        {/* Generate two different hexa colors every time */}
-        <HexaColor />
-        <HexaColor />
-      </div>
-    </div>
-  </main>
-)
-
-// Footer Component
-const Footer = () => (
-  <footer>
-    <div className='footer-wrapper'>
-      <p>Copyright 2020</p>
-    </div>
-  </footer>
-)
+// // Footer Component
+// const Footer = () => (
+//   <footer>
+//     <div className="footer-wrapper">
+//       <p>Copyright 2020</p>
+//     </div>
+//   </footer>
+// );
 
 // The App, or the parent or the container component
 const App = () => (
-  <div className='app'>
-    <Header />
-    <Main />
-    <Footer />
+  <div className="app">
+    <HexaColor />
+    <TechList />
   </div>
-)
+);
 
-const rootElement = document.getElementById('root')
+const rootElement = document.getElementById("root");
 // we render the App component using the ReactDOM package
-ReactDOM.render(<App />, rootElement)
+ReactDOM.render(<App />, rootElement);
